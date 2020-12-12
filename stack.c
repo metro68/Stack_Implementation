cef100276f79030c756d3a5dcc8805f94d7d4a1f
metro68 <59951211+metro68@users.noreply.github.com>
2020-12-12 19:04:31 +0000

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <malloc.h>
+#define MAX 10
+int stack[], top = -1;
+void push(int stack[], int val);
+int peek(int stack[]);
+int pop(int stack[]);
+
+
+void push(int stack[], int val)
+{
+   if(top == MAX-1)
+   {
+     printf("\n STACK OVERFLOW");
+   }
+   else {
+     stack[++top] = val;
+   }
+}
+
+int peek(int stack[]){
+  if(top == -1){	
+    printf("\n STACK IS EMPTY");
+    return -1;
+  }
+  else
+    return (stack[top]);
+}
+
+int pop(int stack[]){
+  if(top == -1){
+    printf("\n STACK IS EMPTY");
+    return -1;
+  }
+  else
+    return (stack[top--]);
+}
